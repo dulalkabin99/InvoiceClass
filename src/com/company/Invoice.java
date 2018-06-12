1d776c074e09af14ec947c3c4131a4da83c69474
Kabin Dulal <dulalkabin99@gmail.com>
2018-06-12 13:51:49 -0400

    Final Version

--- src/com/company/Invoice.java
+++ src/com/company/Invoice.java
@@ -7,65 +7,87 @@ public class Invoice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
-        String name;
         double price;
         int quantity;
-        double total;
+
         double subTotal;
+        String input;
 
-        ArrayList <String> list = new ArrayList<String>(5);
-        //ArrayList<Double>Itemprice =new ArrayList<Double>(5);
-        ArrayList <Double> sTotal = new ArrayList<Double>(5);
+        ArrayList <String> taxableList = new ArrayList<String>();
+        ArrayList <String> nonTaxableList = new ArrayList<String>();
+        ArrayList <Double> taxableTotal = new ArrayList<>();
+        ArrayList <Double> nonTaxTotal = new ArrayList<>();
 
-        //name = sc.nextLine();
-        Item Item1= new Item();
-        Item Item2= new Item();
-        Item Item3= new Item();
-        Item Item4= new Item();
-
-        for (int i=0;i<2;i++) {
+        System.out.print("Enter Tax Rate");
+        Item t = new Item();
+        double ta = sc.nextDouble();
+        t.setTaxRate(ta);
+        System.out.print("Enter the First Item ");
+        input = sc.next();
+while (!input.equalsIgnoreCase("n")){
             Item a = new Item();
-            System.out.println("");
-
-            System.out.print("Enter the Item name or Q to calculate to total ");
-            name = sc.next();
-            a.setItemName(name);
-
+            a.setItemName(input);
 
             System.out.print("Enter the price per item ");
             price = sc.nextDouble();
             a.setPrice(price);
 
-
             System.out.print("Enter the Quantity ");
             quantity = sc.nextInt();
             a.setQuantity(quantity);
 
-            list.add((a.getItemName()+ " Quantity-"+ a.getQuantity()+" Price-" +a.getPrice()));
-            sTotal.add(a.getQuantity()*a.getPrice());
+            System.out.print("Is this Taxable ");
+            input = sc.next();
+             if (input.equalsIgnoreCase("y")) {
 
+        taxableList.add((a.getItemName() + " Quantity-" + a.getQuantity() + " Price-" + a.getPrice()));
+        taxableTotal.add(a.getQuantity() * a.getPrice());
+    }
+    else if (input.equalsIgnoreCase("n")) {
 
+        nonTaxableList.add((a.getItemName() + " Quantity-" + a.getQuantity() + " Price-" + a.getPrice()));
+        nonTaxTotal.add(a.getQuantity() * a.getPrice());
+    }
+
+            System.out.print("Enter the Item name or 'N' to calculate to total ");
+            input = sc.next();
         }
-        double x=0;
 
-        for (int i=0; i<sTotal.size();i++){
-            System.out.print( list.get(i)+ " ");
-            System.out.println("for--->"+ sTotal.get(i));
-            x=x+sTotal.get(i);
+        System.out.println("--------------------Thank You for Shopping-------------------");
 
+
+        double taTotal= Item.taTotal(taxableTotal);
+        double ntaTotal= Item.ntaTotal(nonTaxTotal);
+        double tax = Item.tax(taxableTotal);
+        subTotal=ntaTotal+taTotal;
+        double grandTotal = subTotal + tax;
+
+        System.out.println("---Taxable Items---");
+
+        for (int i=0; i<taxableTotal.size();i++) {
+            System.out.print(taxableList.get(i) + " ");
+            System.out.println(String.format("\tfor---> %.2f" , taxableTotal.get(i)));
         }
+        System.out.println("---------------------------------------");
+        System.out.println ( String.format("Taxable Total %.2f: " , taTotal));
+        System.out.println("---------------------------------------");
 
+
+        System.out.println("---Non Taxable Items---");
+        for (int i=0; i<nonTaxTotal.size();i++) {
+            System.out.print("\t "+ nonTaxableList.get(i) + " ");
+            System.out.println(String.format("\t "+"for---> %.2f", nonTaxTotal.get(i)));
+        }
+        System.out.println("---------------------------------------");
+        System.out.println(String.format("Non Taxable Total %.2f : " , ntaTotal));
 
         System.out.println("---------------------------------------");
-        System.out.println("SubTotal: "+ x);
-        System.out.println("Tax: "+ x*0.06);//Assuming 6 % tax
-        System.out.print("Grand Total: " + (x+ (x*0.06)));
-        System.out.println();
+        System.out.println("---------------------------------------");
 
+        System.out.println(String.format("Sub Total: %.2f", subTotal));
+        System.out.println(String.format("Tax: %.2f", tax));
 
-
-
+        System.out.println(String.format("Total Ammount Due %.2f: ", grandTotal));
 
     }
 
--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -1,12 +1,27 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Item {
 
+        private static Double taxRate;
         private Double price;
         private int quantity;
         private String itemName;
+        double tTotal = 0;
+        double ntTotal = 0;
+
+
+
+        public void setTaxRate(double taxRate) {
+                this.taxRate = taxRate;
+        }
+
+
+        public double getTaxRate() {
+                return taxRate;
+        }
 
         public Double getPrice() {
                 return price;
@@ -32,5 +47,36 @@ public class Item {
                 this.itemName = itemName;
         }
 
+        public static double taTotal(ArrayList<Double> taxableTotal) {
+                double tTotal = 0.0;
+                for (int i = 0; i < taxableTotal.size(); i++) {
+                        tTotal += taxableTotal.get(i);
+                }
+                return tTotal;
+        }
+
+        public static double ntaTotal(ArrayList<Double> nonTaxTotal) {
+                double ntTotal = 0.0;
+                for (int i = 0; i < nonTaxTotal.size(); i++) {
+                        ntTotal += nonTaxTotal.get(i);
+                }
+                return ntTotal;
+        }
+
+        public static double tax(ArrayList<Double> taxableTotal) {
+                double tax = 0.0;
+                for (int i = 0; i < taxableTotal.size(); i++) {
+                        tax += (taxableTotal.get(i) * taxRate);
+                }
+                return tax;
+        }
+
+        public static double subTotal(ArrayList<Double> taxableTotal, ArrayList<Double> nonTaxTotal) {
+                double subTotal = 0;
+                subTotal = (ntaTotal(nonTaxTotal)) + (taTotal(taxableTotal));
+
+                return subTotal;
+        }
 
 }
+
